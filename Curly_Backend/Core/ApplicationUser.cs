2581164975433c6bdc7edfa92686e8f69a5e389c
Mykhailo Tkachenko <tkachenkomichael09@gmail.com>
2023-12-04 11:41:46 +0200

    UPD: application user

--- Curly_Backend/Core/ApplicationUser.cs
+++ Curly_Backend/Core/ApplicationUser.cs
@@ -7,4 +7,6 @@ public class ApplicationUser : IdentityUser<int>
     public required string FirstName { get; set; }
     
     public required string LastName { get; set; }
+    
+    public new required string Email { get => base.Email; set => base.Email = value; }
 }
