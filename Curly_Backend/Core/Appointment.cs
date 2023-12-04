3a4f3cd0d33c493a0065ed95f38881c0272b1cab
Mykhailo Tkachenko <tkachenkomichael09@gmail.com>
2023-12-04 11:41:37 +0200

    UPD: appointment

--- Curly_Backend/Core/Appointment.cs
+++ Curly_Backend/Core/Appointment.cs
@@ -2,15 +2,19 @@ namespace Core;
 
 public class Appointment
 {
-    private DateTime _at;
     public int Id { get; set; }
-
+    
     //TODO: to local time set local in db also
     public DateTime At
     {
         get => _at.ToLocalTime(); 
         set => _at = value.ToUniversalTime();
     }
+    
+    public bool IsCancelled { get; internal set; }
+    
+    public double TotalCost => Favors?.Select(f => f.Cost).DefaultIfEmpty(0).Sum() ?? 0.0;
+    
     
     public DateTime PlacedAt { get; set; }
     
@@ -19,6 +23,7 @@ public class Appointment
     public Client Client { get; set; }
     
     public ICollection<Favor>? Favors { get; set; }
-
-    public double TotalCost => Favors?.Select(f => f.Cost).DefaultIfEmpty(0).Sum() ?? 0.0;
+    
+    
+    private DateTime _at;
 }
